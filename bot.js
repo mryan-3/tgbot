095ddc6bdb868e9f7fdae4aa794dd53e39d5ed66
mryan-3 <malawaryan@gmail.com>
2024-07-19 11:27:47 +0300

    feat(core): add mechanism for sending airdrop through the bot

--- bot.js
+++ bot.js
@@ -1,44 +1,82 @@
 import { Telegraf } from 'telegraf'
+import { HttpAgent, Actor} from '@dfinity/agent'
+import { Principal} from '@dfinity/principal'
 import { message } from 'telegraf/filters'
+import { idlFactory } from './idl.js'
 import 'dotenv/config'
 
 const token = process.env.BOT_TOKEN
+const canisterId = process.env.CANISTER_ID
 
 const bot = new Telegraf(token)
 
-const duckFacts = [
-  'Ducks have been domesticated as pets and farm animals for more than 500 years.',
-  'A duck’s quack doesn’t echo, and no one knows why.',
-  'Ducks are omnivores: they eat plants, insects, small fish, and algae.',
-]
+const agent = new HttpAgent({ host: 'https://ic0.app' })
+const canisterActor = Actor.createActor(idlFactory, {
+    agent: agent,
+    canisterId: canisterId,
+})
+
 
 console.log(`Bot is starting...`)
 bot.start((ctx) =>
   ctx.reply(
-    'Welcome to the DuckBuck Bot! Use /help to see available commands.',
+    'Welcome to the DuckBuck Bot! Please send your principal address to receive an airdrop.',
   ),
 )
+
+
+bot.command('tokenname', async (ctx) => {
+  try {
+    const tokenName = await canisterActor.icrc1_name();
+    ctx.reply(`The token name is: ${tokenName}`);
+  } catch (error) {
+    console.error('Error getting token name:', error);
+    ctx.reply('An error occurred while retrieving the token name. Please try again.');
+  }
+});
+
+bot.on(message('text'), async (ctx) => {
+    const userAddress = ctx.message.text
+
+    try {
+        const account = Principal.fromText(userAddress)
+
+        const transferArgs = {
+            to: {
+                owner: account,
+                subaccount: []
+            },
+            fee: [],
+            memo: [],
+            from_subaccount: [],
+            created_at_time: [],
+            amount: 10
+        }
+
+        const transferResult = await canisterActor.icrc1_transfer(transferArgs)
+        console.log(transferResult)
+
+        if ('OK' in transferResult) {
+            ctx.reply('Your airdrop has been sent successfully!')
+        } else {
+            ctx.reply(`An error occurred while sending the airdrop. ${JSON.stringify(transferResult.Err)}`)
+        }
+    } catch (error) {
+        console.log(`Error while sending airdrop: ${error}`)
+        ctx.reply(`An error occurred while sending the airdrop.`)
+    }
+})
 
 bot.help((ctx) =>
   ctx.reply(
-    '/info - Learn about DuckBuck.\n /air - Get an airdrop. \n /fact - Get a random fact. \n /quit - Leave the chat',
-  ),
-)
-
-bot.command('info', (ctx) =>
-  ctx.reply(
-    `Welcome to DUCKBUCK, the quackiest meme coin in the crypto pond! Inspired by the irreverent and money-loving Howard the Duck, DUCKBUCK is here to bring a splash of fun and a wave of wealth to the crypto community. Our mission? To create a financial revolution that’s as entertaining as a Saturday morning cartoon and as lucrative as a golden egg-laying duck!`,
+    '/air- Let the bot send an airdrop. \n /tokenname - Get the token name. \n /quit - Leave the chat',
   ),
 )
 
 bot.command('air', (ctx) =>
-  ctx.reply('Do you want an airdrop? I will help you in getting the airdrop'),
+  ctx.reply('Do you want an airdrop? I will help you in getting the airdrop. Please send your principal address to receive an airdrop.'),
 )
 
-bot.command('fact', (ctx) => {
-  const fact = duckFacts[Math.floor(Math.random() * duckFacts.length)]
-  ctx.reply(fact)
-})
 
 bot.command('quit', async (ctx) => {
   await ctx.reply('Have a ducky day!')
